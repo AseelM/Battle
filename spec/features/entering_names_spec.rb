b213109f543dec72f28227b52e2d47ff58dbb29a
aseelm <aseelmustafa@outlook.com>
2018-05-05 18:08:13 +0100

    refactors for first user story

--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -3,7 +3,7 @@ feature 'Entering names' do
     visit('/')
     fill_in 'player1', with: 'Bar'
     fill_in 'player2', with: 'Ken'
-    click_button 'Submit'
+    click_button 'SET IT OFF!'
     expect(page).to have_content('Bar vs. Ken')
   end
 end

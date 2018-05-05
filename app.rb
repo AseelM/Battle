34eac10a2b656e431cbc792242b5abc2c816207d
aseelm <aseelmustafa@outlook.com>
2018-05-05 17:57:58 +0100

    creates feature test for entering player names, adds functionality to pass test

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
   get '/' do
-    'heeeelloooo battle'
+    erb :index
+  end
+
+  post '/names' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :play
   end
 
   run! if app_file == $0
---
+++ spec/features/entering_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Entering names' do
+  scenario 'Sumbitting names' do
+    visit('/')
+    fill_in 'player1', with: 'Bar'
+    fill_in 'player2', with: 'Ken'
+    click_button 'Submit'
+    expect(page).to have_content('Bar vs. Ken')
+  end
+end
